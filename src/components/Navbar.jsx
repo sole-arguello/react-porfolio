b91679a0bfba8015e0771cbe85c2d5dc7f6f25f2
Arguello Maria Soledad Eugenia <soledad.ar1@gmail.com>
2024-02-07 18:30:09 -0300

    create navbar component, styles and responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,35 @@
-import React from 'react'
+import { useState } from 'react'
+import { FaBars, FaXmark } from "react-icons/fa6"
 export default function Navbar() {
+
+  const [menuOpen, setMenuOpen] = useState(false)
+
+  const handleMenuToggle = () => {
+    setMenuOpen(!menuOpen)
+  }
+
   return (
     <nav className='navbar'>
-        <a className='navbar__logo' href="/">Porfolio</a>
-        <div className='navbar__menu'>
-            <img className='navbar__menu--icon' src="public/assets/nav/menuIcon.png" alt="nuenu boton" />
-            <ul className='navbar__menu--list'>
-                <li className='navbar__menu--list-item'><a className='navbar__menu--list-item_link' href="#abaout">About</a></li>
-                <li className='navbar__menu--list-item'><a className='navbar__menu--list-item_link' href="#experience">Experience</a></li>
-                <li className='navbar__menu--list-item'><a className='navbar__menu--list-item_link' href="#projects">Projects</a></li>
-                <li className='navbar__menu--list-item'><a className='navbar__menu--list-item_link' href="#contact">Contact</a></li>
+
+        <a className='navbar__title' href="/">Porfolio</a>
+        
+        <div className='navbar__menu' >
+            
+            <div className='navbar__menu--btn' onClick={handleMenuToggle} >
+              { 
+                menuOpen 
+                ? (<FaBars size={30} color="white"/> )
+                : (<FaXmark size={30} color="white"/>) 
+              }
+            </div>
+          
+            <ul className={`navbar__menu--items ${menuOpen ? 'open' : ''}`}  >
+                <li className='navbar__menu--item-list'><a className='navbar__menu--item-list_link' href="#abaout">About</a></li>
+                <li className='navbar__menu--item-list'><a className='navbar__menu--item-list_link' href="#experience">Experience</a></li>
+                <li className='navbar__menu--item-list'><a className='navbar__menu--item-list_link' href="#projects">Projects</a></li>
+                <li className='navbar__menu--item-list'><a className='navbar__menu--item-list_link' href="#contact">Contact</a></li>
             </ul>
+
         </div>
     </nav>
   )
