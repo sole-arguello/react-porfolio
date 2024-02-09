79f5f608b2b041ebee5bf0de2636dd102135a524
Arguello Maria Soledad Eugenia <soledad.ar1@gmail.com>
2024-02-09 00:39:44 -0300

    create hero component

---
+++ src/components/Hero.jsx
@@ -0,0 +1,25 @@
+import { getImageUrl } from "../utils"
+export default function Hero() {
+
+  return (
+    <section className="container__hero">
+
+        <div className="content__hero--container">
+            <div className="content__hero">
+                <h1 className="content__hero--title">Hola, soy Soledad</h1>
+                <div className="content__hero--description">
+                <p >Soy desarrolladora FullStack con 2 años de experiencia 
+                    usando React y NodeJs.</p>
+                <p>¡Comunícate con migo, si quieres saber más de mi! </p>
+                </div>
+                <a className="content__hero--btnContact" href="mailto:soledad.arguello.trabajo@gmail.com">Contactame</a>
+            </div>
+            
+            <img className="image__hero" src={getImageUrl('/public/assets/about/yo.jpeg')} alt="mi imagen" />
+            <div className="topBlur__hero"/>
+            <div className="bottomBlur__hero"/>
+        </div>
+
+    </section>
+  )
+}
