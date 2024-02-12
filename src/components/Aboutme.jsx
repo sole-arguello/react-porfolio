bd1bf341b9073bd54eeec5fc63cdcbdd4765fa48
Arguello Maria Soledad Eugenia <soledad.ar1@gmail.com>
2024-02-12 20:14:16 -0300

    create About Me component and the style file

---
+++ src/components/Aboutme.jsx
@@ -0,0 +1,52 @@
+import { FaArrowPointer, FaDesktop, FaMobileScreenButton } from "react-icons/fa6";
+import { FaServer } from "react-icons/fa";
+export default function Aboutme() {
+  return (
+    <section className="aboutme__container">
+      
+      <div className="aboutme__content--titleImg">
+        <h2 className="aboutme__content--title"> Sobre mi </h2>
+        <img className="aboutme__content--img" src="/public/assets/about/aboutme.png" alt="" />
+      </div>
+
+      <div className="aboutme__content--info">
+        <img className="aboutme__content--info-img" src="/public/assets/about/about.png" alt="imagen se mi" />
+        
+        <ul className="aboutme__content--info-list">
+
+          <li className="aboutme__content--info-list_item" >
+
+            <div className="aboutme__content--info-icons iconsFront">
+              <FaDesktop />
+              <FaMobileScreenButton />
+            </div>
+
+            <div className="aboutme__content--info-text">
+              <h3 className="aboutme__content--info-text_title">FrontEnd Developer</h3>
+              <p className="aboutme__content--info-text_description">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+                Tenetur, exercitationem minima.</p>
+            </div>
+
+          </li>
+
+          <li className="aboutme__content--info-list_item" >
+           
+              <FaServer className="iconsBack"/>           
+
+            <div className="aboutme__content--info-text">
+              <h3 className="aboutme__content--info-text_title">FrontEnd Developer</h3>
+              <p className="aboutme__content--info-text_description">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+                Tenetur, exercitationem minima.</p>
+            </div>
+          </li>
+
+        </ul>
+        
+      </div>
+
+    </section>
+    
+  )
+}
