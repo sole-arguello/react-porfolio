b47c8bfac8d4afbfc2b43d19249a1914c76323ab
Arguello Maria Soledad Eugenia <soledad.ar1@gmail.com>
2024-02-18 23:56:33 -0300

    add contact component for footer

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Hero from './components/Hero'
 import Aboutme from './components/Aboutme'
 import Experience from './components/Experience'
 import Projects from './components/Projects'
+import Contact from './components/Contact'
 
 function App() {
 
@@ -15,6 +16,7 @@ function App() {
       <Aboutme />
       <Experience />
       <Projects/>
+      <Contact />
     </div>
   )
 }
